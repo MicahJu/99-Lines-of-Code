c91c575b7d8215e07dae27cb11c2b8a8d72efa6c
MichaJu <Micahjuwallace@gmail.com>
2021-01-20 10:06:38 -0600

    NinetyNineLinesofCode

---
+++ NinetyNineLinesofCode.js
@@ -0,0 +1,20 @@
+var lines;
+for (counter = 99; counter >= 1; counter = counter - 1) 
+{
+    if (counter == 1) {
+        lines = 'line';
+    } else {
+        lines = 'lines';
+    }
+    console.log(counter+" "+lines+" of code in the file.");
+    if (counter < 99) {
+        console.log("");
+        console.log(counter+" "+lines+" of code in the file.");
+    }
+    console.log(counter+" "+lines+" of code.");
+    console.log("Micah strikes one out.");
+    console.log("Clears it all out.");
+    if (counter == 1) {
+        console.log("No more lines of code in the file.");
+    }
+}
